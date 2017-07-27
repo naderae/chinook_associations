031d4ab9b1575ccacba5a257b81ac7798a68a75e
naderae <naderabouezze93@gmail.com>
2017-07-26 21:00:10 -0400

    done but: commands dont work, last two incomplete

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,4 @@
 class Playlist < ApplicationRecord
   has_and_belongs_to_many :tracks
+  has_many :genres, through: :tracks
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -3,4 +3,7 @@ class Track < ApplicationRecord
   belongs_to :genre
   belongs_to :media_type
   has_and_belongs_to_many :playlists
+
+  has_many :artists, through: :albums
+  belongs_to :album
 end
